b1e63b27ebebe5a9159e12bfeaf13e497393102b
sukhendu2002 <sukhenduguria2017@gmail.com>
2021-09-01 12:46:11 +0530

    add some

--- index.js
+++ index.js
@@ -22,12 +22,10 @@ const DB_URI = process.env.DATABASE;
 //   res.sendFile(path.join(__dirname, "build", "index.html"));
 // });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 mongoose
   .connect(DB_URI, {
